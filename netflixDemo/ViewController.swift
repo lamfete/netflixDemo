4a03aa19872e0d6174bb991eb4047382a75039ec
Agust Lofianto <xiang@agusts-mac.local>
2016-03-08 22:09:00 +0700

    Initial Commit

---
+++ netflixDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  netflixDemo
+//
+//  Created by Agust Lofianto on 3/8/16.
+//  Copyright © 2016 xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
