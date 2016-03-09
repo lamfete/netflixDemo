5368393c1dde0107e837bf9d2d545d3acc9615e4
Agust Lofianto <xiang@Agusts-Mac.local>
2016-03-09 21:28:29 +0700

    first commit

---
+++ netflixDemo/CategoryRow.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryRow.swift
+//  netflixDemo
+//
+//  Created by Agust Lofianto on 3/8/16.
+//  Copyright © 2016 xiang. All rights reserved.
+//
+
+import UIKit
+
+class CategoryRow: UITableViewCell {
+    
+}
+
+extension CategoryRow : UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("videoCell", forIndexPath: indexPath) as UICollectionViewCell
+        return cell
+    }
+}
+
+extension CategoryRow : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let itemsPerRow:CGFloat = 4
+        let hardCodedPadding:CGFloat = 5
+        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+}
